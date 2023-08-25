b247694d8b8d32ec28665c4ec6a7ef893fb184ab
Luke C. Pilling <luke.pilling@gmail.com>
2023-08-25 11:50:26 +0100

    Update to 0.1.1

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# gwasRtools 0.1.1
+
+* Fix internal -log10p calculation
+
 # gwasRtools 0.1.0
 
 * Added a `NEWS.md` file to track changes to the package.
--- R/genetics.R
+++ R/genetics.R
@@ -39,9 +39,12 @@ lambda_gc = function(p) {
 #' @param is_p Logistic. Default=FALSE. Is this a p-value? 
 #' @noRd
 P_neglog10 = function(z, 
-                          is_p=FALSE) {
+                      is_p=FALSE, 
+                      two_sided=TRUE) {
 	if (!is.numeric(z))  stop("z needs to be numeric")
+	if (is_p & two_sided)  z = z/2
 	if (is_p)  z = abs(qnorm(z))
-	neglog10_p=-( 2 + pnorm(-abs( z ), log.p=T) ) / log(10)
+	neglog10_p = abs( ( pnorm(-abs(z), log.p=TRUE) + log(2) ) / log(10) )
 	neglog10_p
 }
+
